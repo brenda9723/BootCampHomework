6ca6bed503b20a47a7a466dea9d16184eed07daa
brenda9723 <brendbritez97@fpuna.edu.py>
2024-07-04 01:00:57 -0400

    Subida de archivos

---
+++ semana2-javascript/index.js
@@ -0,0 +1,13 @@
+// Create a prompt asking for your hometown and alert the hometown
+// Ask for your first name and last name. Alert your full name
+
+let hometown = prompt("Where is your hometown?");
+
+let firtsName = prompt("How is your name?");
+
+let lastName = prompt("How is your last name?");
+
+
+let sentence = "I am " + firtsName + " " + lastName + "and I am from " + hometown;
+
+alert(sentence);
